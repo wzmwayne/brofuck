989f8fe2a95c164a964a53e77037f28c7c5f8e00
wzmwayne <wzmwayne@hotmail.com>
2025-11-29 09:27:55 +0800

    Change RDP user password generation method
    Updated script to create RDP user with a fixed password instead of generating a random one.

--- .github/workflows/scripts/win-create-user.ps1
+++ .github/workflows/scripts/win-create-user.ps1
@@ -1,17 +1,5 @@
-# Create RDP user with strong password
-Add-Type -AssemblyName System.Security
-$charSet = @{
-  Upper   = [char[]](65..90)
-  Lower   = [char[]](97..122)
-  Number  = [char[]](48..57)
-  Special = ([char[]](33..47) + [char[]](58..64) + [char[]](91..96) + [char[]](123..126))
-}
-$rawPassword = @()
-$rawPassword += $charSet.Upper | Get-Random -Count 4
-$rawPassword += $charSet.Lower | Get-Random -Count 4
-$rawPassword += $charSet.Number | Get-Random -Count 4
-$rawPassword += $charSet.Special | Get-Random -Count 4
-$password = -join ($rawPassword | Sort-Object { Get-Random })
+# Create RDP user with fixed password
+$password = "888888888"
 $securePass = ConvertTo-SecureString $password -AsPlainText -Force
 New-LocalUser -Name "vum" -Password $securePass -AccountNeverExpires
 Add-LocalGroupMember -Group "Administrators" -Member "vum"
